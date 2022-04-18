b2ce7e43a68d40fc61bfa8d5eed3f856547cea71
H0R4TI0 <38559023+H0R4TI0@users.noreply.github.com>
2022-04-18 12:52:49 +0800

    Update Task_Scheduling.ino

--- Week 3- Task Scheduling/Task_Scheduling/Task_Scheduling.ino
+++ Week 3- Task Scheduling/Task_Scheduling/Task_Scheduling.ino
@@ -1,9 +1,58 @@
-void setup() {
-  // put your setup code here, to run once:
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const BaseType_t app_cpu = 1;
+#endif
 
+// LED rates
+static const int rate_1 = 500;  // ms
+static const int rate_2 = 300;  // ms
+
+// Pins
+static const int led_pin = LED_BUILTIN;
+
+// Our task: blink an LED at one rate
+void toggleLED_1(void *parameter) {
+  while(1) {
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(rate_1 / portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(rate_1 / portTICK_PERIOD_MS);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Our task: blink an LED at another rate
+void toggleLED_2(void *parameter) {
+  while(1) {
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(rate_2 / portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(rate_2 / portTICK_PERIOD_MS);
+  }
+}
 
+void setup() {
+
+  // Configure pin
+  pinMode(led_pin, OUTPUT);
+
+  // Task to run forever
+  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
+              toggleLED_1,  // Function to be called
+              "Toggle 1",   // Name of task
+              1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
+              NULL,         // Parameter to pass to function
+              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+              NULL,
+              app_cpu);
+
+  // Task to run forever
+  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
+              toggleLED_2,  // Function to be called
+              "Toggle 2",   // Name of task
+              1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
+              NULL,         // Parameter to pass to function
+              1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+              NULL,
+              app_cpu);
 }
