0218f1765977c9aab25820071cb7eb41053832ba
H0R4TI0 <38559023+H0R4TI0@users.noreply.github.com>
2022-04-18 12:54:59 +0800

    Update Memory_Management.ino

--- Week 4- Memory Management/Memory_Management/Memory_Management.ino
+++ Week 4- Memory Management/Memory_Management/Memory_Management.ino
@@ -1,9 +1,66 @@
-void setup() {
-  // put your setup code here, to run once:
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const BaseType_t app_cpu = 1;
+#endif
 
+// LED rates
+int rate = 500;  // ms
+
+//Serial
+int incomingByte = 0;
+
+// Pins
+static const int led_pin = LED_BUILTIN;
+
+// Our task: blink an LED at one rate
+void toggleLED_1(void *parameter) {
+  while (1) {
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(rate / portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(rate / portTICK_PERIOD_MS);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Our task: Read serial input
+void ReadSerial(void *parameter) {
+  while (1) {
+    if (Serial.available() > 0) {
+      // read the incoming byte:
+      incomingByte = Serial.parseInt(); //read next valid integer, if timeout returns 0
+      if (incomingByte > 0)
+      {
+        rate = incomingByte;
+        Serial.print("Rate: ");
+        Serial.println(rate);
+      }
+    }
+  }
+}
 
+void setup() {
+  Serial.begin(115200);
+  // Configure pin
+  pinMode(led_pin, OUTPUT);
+
+  // Task to run forever
+  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
+    toggleLED_1,  // Function to be called
+    "Toggle 1",   // Name of task
+    1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,
+    app_cpu);
+
+  // Task to run forever
+  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
+    ReadSerial,  // Function to be called
+    "Read Serial",   // Name of task
+    1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,
+    app_cpu);
 }
