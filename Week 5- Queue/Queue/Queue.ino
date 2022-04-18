7e3aa219614bf413cdad3f262be16ff36af8ff21
H0R4TI0 <38559023+H0R4TI0@users.noreply.github.com>
2022-04-18 13:00:30 +0800

    Update Queue.ino

--- Week 5- Queue/Queue/Queue.ino
+++ Week 5- Queue/Queue/Queue.ino
@@ -1,9 +1,82 @@
-void setup() {
-  // put your setup code here, to run once:
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const BaseType_t app_cpu = 1;
+#endif
 
+// Globals
+static char message[255];
+static char c;
+static int i, len = 0;
+static char *msg_ptr = NULL;
+static volatile bool msg_flag = 0;
+
+// Our task: Read serial input and allocate memory
+void ReadSerial(void *parameter) {
+  while (1) {
+    if ((msg_flag == 0) && (Serial.available()))
+    {
+      c = Serial.read();
+      message[i] = c;
+      i++;
+      if (c == '\n')
+      {
+        len = i;
+        i = 0;
+        message[len - 1] = '\0';
+        msg_ptr = (char *)pvPortMalloc(len * sizeof(char));
+
+        // If malloc returns 0 (out of memory), throw an error and reset
+        configASSERT(msg_ptr);
+
+        // Copy message
+        memcpy(msg_ptr, message, len);
+
+        // Notify other task that message is ready
+        msg_flag = 1;
+      }
+    }
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Our task: Print message input by user and clear memory
+void EchoMessage(void *parameter) {
+  while (1) {
+    if (msg_flag == 1)
+    {
+      Serial.println(msg_ptr);
+      Serial.print("Free heap (bytes): ");
+      Serial.println(xPortGetFreeHeapSize());
 
+      // Free buffer, set pointer to null, and clear flag
+      vPortFree(msg_ptr); // Without this, the heap memory will build up with each serial input.
+      msg_ptr = NULL;
+      msg_flag = 0;
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  // Task to run forever
+  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
+    ReadSerial,  // Function to be called
+    "Read Serial",   // Name of task
+    1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,
+    app_cpu);
+
+  // Task to run forever
+  xTaskCreatePinnedToCore(  // Use xTaskCreate() in vanilla FreeRTOS
+    EchoMessage,  // Function to be called
+    "Echo Message",   // Name of task
+    1024,         // Stack size (bytes in ESP32, words in FreeRTOS)
+    NULL,         // Parameter to pass to function
+    1,            // Task priority (0 to configMAX_PRIORITIES - 1)
+    NULL,
+    app_cpu);
 }
